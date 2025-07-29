547c51c5978f99ffe1b9efcab281ceb30753764d
Harsha911x <harsha.02x@gmail.com>
2025-07-29 18:50:50 +0530

    First Spring boot webApp (Note: not connected to DB) Implemented MVC.

---
+++ JobApp/src/main/java/com/example/JobApp/service/JobService.java
@@ -0,0 +1,28 @@
+package com.example.JobApp.service;
+
+import com.example.JobApp.model.JobPost;
+import com.example.JobApp.repo.JobRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class JobService {
+
+    @Autowired
+    private JobRepo service;
+
+    public void addJob(JobPost jobPost){
+        service.addJob(jobPost);
+
+    }
+
+    public List<JobPost> getAllJobs(){
+
+        return service.getAllJobs();
+    }
+
+
+}
